9bd1b01781e90a5e9521a51424b8de350da00550
codyarvonen <codyarvonen@gmail.com>
2023-05-09 15:49:06 -0600

    training should be working now

--- memory.py
+++ memory.py
@@ -9,6 +9,9 @@ class Experience:
         self.reward = reward
         self.next_state = next_state
 
+    def __str__(self):
+        return f"State:\n{self.state}\nAction: {self.action}\nReward: {self.reward}\nNext State:\n{self.next_state}\n\n"
+
 
 class ReplayMemory:
     def __init__(self, size):
@@ -16,6 +19,12 @@ class ReplayMemory:
 
     def __len__(self):
         return len(self.memory)
+
+    def __str__(self):
+        output_string = ""
+        for exp in self.memory:
+            output_string += str(exp)
+        return output_string
 
     def sample(self, batch_size):
         return random.sample(self.memory, batch_size)
